34c2888970b9f2ac736154d8dd5f8a29b632cc8e
Tiffany Chen <tchen@thoughtworks.com>
2017-10-28 13:03:27 -0400

    Rescue bunny connection in event publisher.

--- app/services/event_publisher.rb
+++ app/services/event_publisher.rb
@@ -5,10 +5,10 @@ class EventPublisher
   end
 
   def publish
-    queue = channel.queue(@queue_name)
+    queue = channel.queue(@queue_name, :durable => true)
     exchange = channel.default_exchange
-    exchange.publish(@event.to_json, {:routing_key => queue.name,
-                                      :content_type => 'application/json'})
+    exchange.publish(@event.to_json, { :routing_key => queue.name,
+                                       :content_type => 'application/json' })
     @connection.close
   end
 
@@ -17,13 +17,7 @@ class EventPublisher
   end
 
   def connection
-    begin
-      @connection = Bunny.new(ENV['MESSAGE_QUEUE_URL'])
-      @connection.start
-    rescue Bunny::TCPConnectionFailed => e
-      Rails.logger.warn "Connection to #{ENV['MESSAGE_QUEUE_URL']}failed"
-    rescue Bunny::PossibleAuthenticationFailureError => e
-      Rails.logger.warn "Could not authenticate as #{@connection.username}"
-    end
+    @connection = Bunny.new(ENV['MESSAGE_QUEUE_URL'])
+    @connection.start
   end
 end
--- config/initializers/sneakers.rb
+++ config/initializers/sneakers.rb
@@ -2,14 +2,13 @@ require 'sneakers'
 require 'sneakers/handlers/maxretry'
 
 Sneakers.configure(:amqp => ENV['MESSAGE_QUEUE_URL'],
-                   :retry_timeout => 60 * 1000,
                    :ack => true,
                    :timeout_job_after => 5,
                    :prefetch => 10,
                    :threads => 10,
                    :durable => true,
                    :handler => Sneakers::Handlers::Maxretry,
-                   :retry_max_times => 10
-)
+                   :retry_max_times => 10,
+                   :retry_timeout => 60 * 1000)
 
 Sneakers.logger.level = Logger::INFO
